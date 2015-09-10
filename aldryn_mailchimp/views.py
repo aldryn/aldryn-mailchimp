1278b2048e464b66885a3b98182abf72f5835eae
Martin Koistinen <mkoistinen@gmail.com>
2015-09-10 16:31:42 -0400

    Mark string for translation

--- aldryn_mailchimp/views.py
+++ aldryn_mailchimp/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.contrib import messages
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
